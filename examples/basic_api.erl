1f382798ca3c6d13c26ac4a822a06e6bc532104c
jsz4n <jsz4n+git@ik.me>
2025-09-28 10:37:18 +0200

    add basic example

---
+++ examples/basic_api.erl
@@ -0,0 +1,18 @@
+-module(basic_api).
+-export([start/0]).
+
+start() ->
+    wade_app:start(),
+    wade:route(get, "/hello/[name]", fun(Req) ->
+        Name = wade:param(Req, name),
+        {200, "\{\"message\":\"Hello " ++ Name ++ "!\"}", [{"Content-Type", "application/json"}]}
+    end, []),
+    wade:route(get, "/api/users", fun(_Req) ->
+        {200, "\{\"users\":[\"Bob\", \"Charlie\", \"Alice\"]\}", [{"Content-Type", "application/json"}]}
+    end, []),
+    wade:route(post, "/api/users", fun(Req) ->
+        Name = wade:body(Req, name),
+        {200, [
+            "{\"message\": \"User "++ Name++ " created\"}"
+        ], [{"Content-Type", "application/json"}]}
+    end, [name]).
